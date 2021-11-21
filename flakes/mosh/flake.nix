5d2d653c54e3d5f4e64452ca760c7a7c90049b79
Andrew Sutherland <sutherlanda@gmail.com>
2021-11-20 16:35:56 -0800

    fix macOS specific issue with glibcLocales

--- flakes/mosh/flake.nix
+++ flakes/mosh/flake.nix
@@ -28,15 +28,18 @@
             ]
             ++ (with perlPackages; [ perl IOTty ])
             ++ lib.optional final.stdenv.isLinux libutempter;
+
             configurePhase = ''
-              ./autogen.sh
-              ./configure
+              ./autogen.sh;
+              ./configure;
             '';
-            installPhase = ''
+
+            installPhase = '' 
               make prefix=$out install;
-              wrapProgram $out/bin/mosh --prefix PERL5LIB : $PERL5LIB --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive;
-              wrapProgram $out/bin/mosh-server --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive;
-            '';
+              wrapProgram $out/bin/mosh --prefix PERL5LIB : $PERL5LIB; 
+            ''
+            + final.lib.strings.optionalString (glibcLocales != null)
+              "wrapProgram $out/bin/mosh-server --set LOCALE_ARCHIVE ${glibcLocales}/lib/locale/locale-archive;";
           };
         };
         pkgs = import nixpkgs { inherit system; overlays = [ mosh-overlay ]; };
