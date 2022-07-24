39bf8b9b8cc371bc2600f195fcfd556fbd47188c
Andrew Sutherland <sutherlanda@gmail.com>
2022-07-24 11:58:17 -0600

    Cleanup of config folders

--- modules/cli.nix
+++ modules/cli.nix
@@ -78,8 +78,7 @@
   };
 
   home.file.".tmux.conf".source = ../config/tmux/tmux.conf;
-  home.file.".config/tmux/themes/tokyonight.conf".source = ../config/tmux/themes/tokyonight.conf;
-  home.file.".config/tmux/themes/nightfox.conf".source = ../config/tmux/themes/nightfox.conf;
+  home.file.".config/tmux".source = ../config/tmux;
   home.file.".config/ranger/rifle.conf".source = ../config/ranger/rifle.conf;
   home.file.".config/ranger/rc.conf".source = ../config/ranger/rc.conf;
   home.file.".zshenv".text = ''
