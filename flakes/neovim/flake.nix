d0bc530cc0b15719cd0fd8449e747867936efa4f
Andrew Sutherland <sutherlanda@gmail.com>
2023-11-10 22:33:16 -0600

    haskell-tools

--- flakes/neovim/flake.nix
+++ flakes/neovim/flake.nix
@@ -30,6 +30,10 @@
       url = "github:edgedb/edgedb-vim";
       flake = false;
     };
+    haskell-tools = {
+      url = "github:mrcjkb/haskell-tools.nvim";
+      flake = false;
+    };
 
     # Syntax highlighting
     vim-nix = {
@@ -183,6 +187,7 @@
         "nvim-typescript"
         "null-ls"
         "edgedb-vim"
+        "haskell-tools"
         "rust-tools"
         "vim-nix"
         "vim-glsl"
