b05d56794c0ad831a29818560f69549ada78d2f3
Andrew Sutherland <sutherlanda@gmail.com>
2023-11-28 22:23:44 -0600

    .

--- flakes/neovim/config/lua/global.lua
+++ flakes/neovim/config/lua/global.lua
@@ -24,10 +24,10 @@ require('neoscroll').setup({})
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
-t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '450'}}
-t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '450'}}
-t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
-t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-b>'] = {'scroll', {'-vim.wo.scroll', 'true', '450'}}
+t['<C-f>'] = {'scroll', { 'vim.wo.scroll', 'true', '450'}}
+t['<C-u>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-d>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}}
 t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
 t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
 t['zt']    = {'zt', {'250'}}
