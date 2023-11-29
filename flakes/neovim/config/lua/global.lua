e92dcf3103c64bfba83ff815762c8faa3b0c116f
Andrew Sutherland <sutherlanda@gmail.com>
2023-11-28 22:19:59 -0600

    update smooth scroll key bindings

--- flakes/neovim/config/lua/global.lua
+++ flakes/neovim/config/lua/global.lua
@@ -19,7 +19,22 @@ vim.g.loaded_ruby_provider = 0
 
 require('alpha').setup(require('alpha.themes.startify').opts)
 
+-- Smooth scrolling setup
 require('neoscroll').setup({})
+
+local t = {}
+-- Syntax: t[keys] = {function, {function arguments}}
+t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '450'}}
+t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '450'}}
+t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
+t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
+t['zt']    = {'zt', {'250'}}
+t['zz']    = {'zz', {'250'}}
+t['zb']    = {'zb', {'250'}}
+
+require('neoscroll.config').set_mappings(t)
 
 -- Lualine
 require'lualine'.setup({
