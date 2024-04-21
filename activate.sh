b85379c9da86860f78d7475624d84284b994adea
Andrew Sutherland <sutherlanda@gmail.com>
2024-04-20 20:37:59 -0600

    fix typo

--- activate.sh
+++ activate.sh
@@ -7,6 +7,6 @@ case "$1" in
   "darwin-m1") nix run --impure .#darwin-m1 "$@";;
   "debian") nix run --impure .#debian "$@";;
   *)
-    "usage: activate {darwin-m1,debian}"
+    echo "usage: activate {darwin-m1,debian}"
     exit 1;;
 esac
