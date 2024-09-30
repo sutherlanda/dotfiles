d9929ec894d64274bd1cc75bc165745a5d278a95
Andrew Sutherland <sutherlanda@gmail.com>
2024-09-29 18:55:34 -0600

    update

--- activate.sh
+++ activate.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-nix build ./flakes/neovim
-nix flake lock --update-input neovim-flake
-nix build ./flakes/mosh
-nix flake lock --update-input mosh-flake
-
 case "$1" in
   "darwin-m1") nix run --impure .#darwin-m1 "$@";;
   "debian") nix run --impure .#debian "$@";;
